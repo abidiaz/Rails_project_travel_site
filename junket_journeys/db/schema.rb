8879280a53a7dbfff2a6504661959f786eafd3ce
Charlie Cook <turingbombe@gmail.com>
2016-10-11 17:59:52 -0400

    changed db from SQL to Postgres

--- junket_journeys/db/schema.rb
+++ junket_journeys/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826204140) do
+ActiveRecord::Schema.define(version: 20160826204142) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "attractions", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160826204140) do
     t.integer  "city_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["city_id"], name: "index_attractions_on_city_id"
+    t.index ["city_id"], name: "index_attractions_on_city_id", using: :btree
   end
 
   create_table "cities", force: :cascade do |t|
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20160826204140) do
     t.integer  "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["country_id"], name: "index_cities_on_country_id"
+    t.index ["country_id"], name: "index_cities_on_country_id", using: :btree
   end
 
   create_table "countries", force: :cascade do |t|
@@ -43,8 +46,8 @@ ActiveRecord::Schema.define(version: 20160826204140) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["attraction_id"], name: "index_reviews_on_attraction_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["attraction_id"], name: "index_reviews_on_attraction_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +57,8 @@ ActiveRecord::Schema.define(version: 20160826204140) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "attractions", "cities"
+  add_foreign_key "cities", "countries"
+  add_foreign_key "reviews", "attractions"
+  add_foreign_key "reviews", "users"
 end
